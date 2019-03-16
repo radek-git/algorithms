cf4acb5efb961897ca4d3e36661c43744cadab97
radek-git <radoslaw.szpionkowski@gmail.com>
2019-03-16 14:35:20 +0100

    Maskify

---
+++ src/main/java/_31_Maskify.java
@@ -0,0 +1,20 @@
+public class _31_Maskify {
+
+    public static void main(String[] args) {
+
+        String s = "23";
+
+        System.out.println(maskify(s));
+    }
+
+
+
+    public static String maskify(String str) {
+        if (str.length() <= 4) return str;
+        String result = "";
+        for (int i = 0; i < str.length()-4; i++) {
+            result += "#";
+        }
+        return result + str.substring(str.length()-4);
+    }
+}
