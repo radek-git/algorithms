229ff0ed25eafa0485ea134c75acb8542ad5ea05
radek-git <radoslaw.szpionkowski@gmail.com>
2019-03-19 23:29:55 +0100

    PerfectNumber

---
+++ src/main/java/_38_PerfectNumber.java
@@ -0,0 +1,21 @@
+public class _38_PerfectNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(perfectNumber(6));
+    }
+
+    public static boolean perfectNumber(int number) {
+        if (number < 6) return false;
+
+        int sum = 0;
+        for (int i = 1; i <= number/2; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        return number == sum;
+
+    }
+}
