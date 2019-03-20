ad6c133388e36ac1531a393f033e09a64760b42e
radek-git <radoslaw.szpionkowski@gmail.com>
2019-03-20 18:42:51 +0100

    DontGiveMeFive

---
+++ src/main/java/_41_DontGiveMeFive.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _41_DontGiveMeFive {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(dontGiveMeFive(2,9));
+    }
+
+    public static int dontGiveMeFive(int start, int end) {
+        int sum = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            list.add(i);
+        }
+        sum = list.stream().mapToInt(Integer::intValue).sum()-5;
+
+        return sum;
+    }
+
+}
