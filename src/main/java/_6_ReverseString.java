20f7d3960e60839847bb1e34b278658241009f58
radek-git <radoslaw.szpionkowski@gmail.com>
2019-04-06 22:57:52 +0200

    CompareStringBySumOfChars

--- src/main/java/_6_ReverseString.java
+++ src/main/java/_6_ReverseString.java
@@ -7,6 +7,8 @@ public class _6_ReverseString {
     public static String reverseString (String str){
         StringBuilder sb = new StringBuilder();
 
+
+
         for(int i = str.length()-1; i>=0; i--){
             sb.append(str.charAt(i));
         }
